f4787c34d8a4aa68b02f5675052a2fbc518c358a
arnaud sellenet <arnodmental@gmail.com>
2012-10-20 02:21:16 +0200

    small refactoring

--- lib/bank_validator/bank_validator.rb
+++ lib/bank_validator/bank_validator.rb
@@ -8,10 +8,10 @@ module BankValidator
     end
 
     def validate(record)
-      record.errors.add :iban, record.errors.generate_message(:iban, :blank) unless @allow_nil || record.iban.present?
-      record.errors.add :bic, record.errors.generate_message(:bic, :blank) unless @allow_nil || record.bic.present?
+      add_error(record, :iban, :blank) unless @allow_nil || record.iban.present?
+      add_error(record, :bic, :blank) unless @allow_nil || record.bic.present?
       if record.respond_to?(:iban) && record.respond_to?(:bic)
-        record.errors.add :iban, record.errors.generate_message(:iban, :none_or_both_bic_iban) unless (record.iban.present? == record.bic.present?)
+        add_error(record, :iban, :none_or_both_bic_iban) unless (record.iban.present? == record.bic.present?)
       end
       validate_iban(record) if record.respond_to?(:iban) && record.iban.present?
       validate_bic(record) if record.respond_to?(:bic) && record.bic.present?
@@ -19,15 +19,19 @@ module BankValidator
 
     private
 
+    def add_error(record, field, message_key)
+      record.errors.add field, record.errors.generate_message(field, message_key)
+    end
+
     def validate_iban(record)
       # IBAN code should start with country code (2letters)
-      record.errors.add :iban, record.errors.generate_message(:iban, :country_code_missing_in_iban) and return unless record.iban.to_s =~ /^[A-Z]{2}/i
+      return add_error(record, :iban, :country_code_missing_in_iban) unless record.iban.to_s =~ /^[A-Z]{2}/i
       iban = record.iban.gsub(/ /,'').gsub(/[A-Z]/) { |p| (p.respond_to?(:ord) ? p.ord : p[0]) - 55 }
-      record.errors.add :iban, record.errors.generate_message(:iban, :invalid) unless (iban[6..iban.length-1].to_s+iban[0..5].to_s).to_i % 97 == 1
+      add_error(record, :iban, :invalid) unless (iban[6..iban.length-1].to_s+iban[0..5].to_s).to_i % 97 == 1
     end
 
     def validate_bic(record)
-      record.errors.add :bic, record.errors.generate_message(:bic, :invalid) unless /^([a-zA-Z]{4}[a-zA-Z]{2}[a-zA-Z0-9]{2}([a-zA-Z0-9]{3})?)$/.match(record.bic)
+      add_error(record, :iban, :invalid) unless /^([a-zA-Z]{4}[a-zA-Z]{2}[a-zA-Z0-9]{2}([a-zA-Z0-9]{3})?)$/.match(record.bic)
     end
   end
 end
--- spec/bank_validator_spec.rb
+++ spec/bank_validator_spec.rb
@@ -5,139 +5,160 @@ describe BankValidator::Validator do
   A_VALID_BIC = 'AGRIFRPP812'
   A_VALID_IBAN = 'FR76 1120 6200 1012 1495 4641 151'
 
-  def mock_with(fields = {})
-    mock(fields.merge errors: mock(add: nil, generate_message: nil))
-  end
+  let(:bankvalidator) { BankValidator::Validator.new(options) }
+  let(:options) { {} }
 
-  describe 'routing validation' do
-    let(:options) { {} }
-    let(:bankvalidator) { BankValidator::Validator.new(options) }
-    subject { bankvalidator }
-
-    context "when has an iban" do
-      let(:record) { mock_with(iban: 'FR123') }
-      it "uses validate_iban" do
-        subject.should_receive(:validate_iban)
-        subject.validate(record)
-      end
+  describe '#add_error' do
+    let(:errors) { mock(add: nil, generate_message: nil) }
+    let(:record) { mock(errors: errors) }
+    before do
+      errors.stub(:generate_message).with(:a_field, :a_message).and_return 'an_error_string'
     end
-    context "when has not an iban" do
-      let(:record) { mock_with }
-      it "doesnt uses validate_iban" do
-        subject.should_not_receive(:validate_iban)
-        subject.validate(record)
-      end
-    end
-    context "when has a bic" do
-      let(:record) { mock_with(bic: 'FR123') }
-      it "uses validate_bic" do
-        subject.should_receive(:validate_bic)
-        subject.validate(record)
-      end
-    end
-    context "when has not an bic" do
-      let(:record) { mock_with }
-      it "doesnt use validate_iban" do
-        subject.should_not_receive(:validate_bic)
-        subject.validate(record)
-      end
+    it "adds an error message to the record" do
+      record.should_receive(:errors)
+      errors.should_receive(:add).with :a_field, 'an_error_string'
+      bankvalidator.send(:add_error, record, :a_field, :a_message)
     end
   end
 
-  describe '#validate_iban' do
-    let(:options) { {} }
-    subject { BankValidator::Validator.new(options).send(:validate_iban, record) }
-    let(:record) { mock_with(iban: iban) }
-    [ 'F76 1120 6200 1012 1495 4641', 'FR76 1120 6200 1012 1495 4641', '93','1234' ].each do |invalid_iban|
-      context "iban: #{invalid_iban}" do
-        let(:iban) { invalid_iban }
-        it "is not valid" do
-          record.should_receive :errors
-          subject
+  describe 'validation process' do
+    before(:all) { BankValidator::Validator.stub(add_error: nil) }
+    describe 'routing validation' do
+      let(:options) { {} }
+
+      subject { bankvalidator }
+
+      context "when has an iban" do
+        let(:record) { mock(iban: 'FR123') }
+        it "uses validate_iban" do
+          subject.should_receive(:validate_iban)
+          subject.validate(record)
+        end
+      end
+
+      context "when has not an iban" do
+        let(:record) { mock }
+        it "doesnt uses validate_iban" do
+          subject.should_not_receive(:validate_iban)
+          subject.validate(record)
+        end
+      end
+
+      context "when has a bic" do
+        let(:record) { mock(bic: 'FR123') }
+        it "uses validate_bic" do
+          subject.should_receive(:validate_bic)
+          subject.validate(record)
+        end
+      end
+
+      context "when has not an bic" do
+        let(:record) { mock }
+        it "doesnt use validate_iban" do
+          subject.should_not_receive(:validate_bic)
+          subject.validate(record)
         end
       end
     end
-    [ 'FR7611206200101214954641151', 'FR76 1120 6200 1012 1495 4641 151' ].each do |valid_iban|
-      context "iban: #{valid_iban}" do
-        let(:iban) { valid_iban }
+
+    describe '#validate_iban' do
+      subject { bankvalidator.send(:validate_iban, record) }
+      let(:record) { mock(iban: iban) }
+
+      [ 'F76 1120 6200 1012 1495 4641', 'FR76 1120 6200 1012 1495 4641', '93','1234' ].each do |invalid_iban|
+        context "iban: #{invalid_iban}" do
+          let(:iban) { invalid_iban }
+          it "is not valid" do
+            bankvalidator.should_receive(:add_error)
+            subject
+          end
+        end
+      end
+
+      [ 'FR7611206200101214954641151', 'FR76 1120 6200 1012 1495 4641 151' ].each do |valid_iban|
+        context "iban: #{valid_iban}" do
+          let(:iban) { valid_iban }
+          it "is valid" do
+            bankvalidator.should_not_receive :add_error
+            subject
+          end
+        end
+      end
+    end
+
+    describe '#validate_bic' do
+      subject { bankvalidator.send(:validate_bic, record) }
+      let(:record) { mock(bic: bic) }
+      [ '12RIFRPP812', 'AGRIFRPP81', 'AGRI23PP812' ].each do |invalid_bic|
+        context "bic: #{invalid_bic}" do
+          let(:bic) { invalid_bic }
+          it "is not valid" do
+            bankvalidator.should_receive :add_error
+            subject
+          end
+        end
+      end
+
+      [ A_VALID_BIC ].each do |valid_bic|
+        context "bic: #{valid_bic}" do
+          let(:bic) { valid_bic }
+          it "is valid" do
+            bankvalidator.should_not_receive :add_error
+            subject
+          end
+        end
+      end
+    end
+
+    describe 'requiring both or none' do
+      subject { bankvalidator.validate(record) }
+      let(:record) { mock(iban: iban, bic: bic) }
+
+      context "when both are missing" do
+        let(:bic) { nil }
+        let(:iban) { nil }
         it "is valid" do
-          record.should_not_receive :errors
+          bankvalidator.should_not_receive :add_error
           subject
         end
       end
-    end
-  end
-  describe '#validate_bic' do
-    let(:options) { {} }
-    subject { BankValidator::Validator.new(options).send(:validate_bic, record) }
-    let(:record) { mock_with(bic: bic) }
-    [ '12RIFRPP812', 'AGRIFRPP81', 'AGRI23PP812' ].each do |invalid_bic|
-      context "bic: #{invalid_bic}" do
-        let(:bic) { invalid_bic }
+
+      context "when both are missing and allow_nil set to false" do
+        let(:options) { {allow_nil: false} }
+        let(:bic) { nil }
+        let(:iban) { nil }
         it "is not valid" do
-          record.should_receive :errors
+          bankvalidator.should_receive(:add_error).twice
           subject
         end
       end
-    end
-    [ A_VALID_BIC ].each do |valid_bic|
-      context "bic: #{valid_bic}" do
-        let(:bic) { valid_bic }
+
+      context "when both are valid and allow_nil set to false" do
+        let(:options) { {allow_nil: false} }
+        let(:bic) { A_VALID_BIC }
+        let(:iban) { A_VALID_IBAN }
         it "is valid" do
-          record.should_not_receive :errors
+          bankvalidator.should_not_receive :add_error
           subject
         end
       end
-    end
-  end
-  describe 'requiring both or none' do
-    let(:options) { {} }
-    let(:bankvalidator) { BankValidator::Validator.new(options) }
-    subject { bankvalidator.validate(record) }
-    let(:record) { mock_with(iban: iban, bic: bic) }
-    context "when both are missing" do
-      let(:bic) { nil }
-      let(:iban) { nil }
-      it "is valid" do
-        record.should_not_receive(:errors)
-        subject
-      end
-    end
 
-    context "when both are missing and allow_nil set to false" do
-      let(:options) { {allow_nil: false} }
-      let(:bic) { nil }
-      let(:iban) { nil }
-      it "is not valid" do
-        record.should_receive(:errors)
-        subject
+      context "when bic is missing and iban is valid" do
+        let(:bic) { nil }
+        let(:iban) { A_VALID_IBAN }
+        it "is not valid" do
+          bankvalidator.should_receive :add_error
+          subject
+        end
       end
-    end
-    context "when both are valid and allow_nil set to false" do
-      let(:options) { {allow_nil: false} }
-      let(:bic) { A_VALID_BIC }
-      let(:iban) { A_VALID_IBAN }
-      it "is valid" do
-        record.should_not_receive(:errors)
-        subject
-      end
-    end
 
-    context "when bic is missing and iban is valid" do
-      let(:bic) { nil }
-      let(:iban) { A_VALID_IBAN }
-      it "is not valid" do
-        record.should_receive(:errors)
-        subject
-      end
-    end
-
-    context "when iban is missing and bic is valid" do
-      let(:iban) { nil }
-      let(:bic) { A_VALID_BIC }
-      it "is not valid" do
-        record.should_receive(:errors)
-        subject
+      context "when iban is missing and bic is valid" do
+        let(:iban) { nil }
+        let(:bic) { A_VALID_BIC }
+        it "is not valid" do
+          bankvalidator.should_receive :add_error
+          subject
+        end
       end
     end
   end
